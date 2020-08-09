01d071e79acdc0602983d26aeedc0bd8b0f58e7b
yuka-i0412 <yuka.i@di-v.co.jp>
2020-08-09 12:30:09 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
   def index 
-    @posts = Post.all # 全てのレコードを@postsに代入
+    @posts = Post.all.order(id: "DESC")
+     # 全てのレコードを@postsに代入+降順で表示
   end
   
-  def new
-  end
+  #削除
+  #def new
+  #end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
